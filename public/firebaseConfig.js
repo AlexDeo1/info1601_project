3df041ee3c52880fe98cb56f4dfe696bbed44bc0
Alex Deo <alex.deo2004@gmail.com>
2025-04-21 05:30:01 -0400

    clean up and fixed firebase

---
+++ public/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC6NF4Pn6wH8I0ZXf23f546o8wpG3qdlRk",
+    authDomain: "info1601-project-ea162.firebaseapp.com",
+    projectId: "info1601-project-ea162",
+    storageBucket: "info1601-project-ea162.firebasestorage.app",
+    messagingSenderId: "793666674384",
+    appId: "1:793666674384:web:258a69f1206d9a662f08d5"
+  };
+
+  export default firebaseConfig;
